acb686272a5628ab4e5107dfb97f6b8cac294fc2
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2023-11-24 21:37:55 +0100

    Update 7.3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,15 +3,6 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
-
-player.on('play', function () {
-  console.log('played the video!');
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-
 player.on(
   'timeupdate',
   throttle(event => {
