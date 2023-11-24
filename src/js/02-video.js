891a2595b78757068f2b8be940f0d82f32e14ea5
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2023-11-24 21:28:31 +0100

    Update 7.1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,25 @@
-import Player from "@vimeo/player";
-import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo.Player(iframe);
-player.on('timeupdate', throttle((event) => {
-    localStorage.setItem("videoplayer-current-time", event.seconds);
-}, 1000));
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on(
+  'timeupdate',
+  throttle(event => {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+  }, 1000)
+);
 
 window.addEventListener('DOMContentLoaded', () => {
-    const time = localStorage.getItem("videoplayer-current-time");
-    player.setCurrentTime(time);
-}); 
+  const time = localStorage.getItem('videoplayer-current-time');
+  player.setCurrentTime(time);
+});
