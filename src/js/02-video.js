d979cd0d37ae862aea2db4dfb3cd630e06e99c6e
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2023-11-24 23:24:15 +0100

    Update 7.8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,14 +3,20 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
+
 player.on(
   'timeupdate',
   throttle(event => {
     localStorage.setItem('videoplayer-current-time', event.seconds);
   }, 1000)
 );
-
+const time = localStorage.getItem('videoplayer-current-time');
 window.addEventListener('DOMContentLoaded', () => {
-  const time = localStorage.getItem('videoplayer-current-time');
   player.setCurrentTime(time);
 });
+
+if(time !== null){
+  console.log(`currentTime is: ${time}`);
+} else{
+  console.log(`current time is: ${null}`);
+}
