907143654163a9807d3442f2899a7a138dfbc961
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2023-11-24 21:29:56 +0100

    Update 7.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,7 +2,7 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 player.on('play', function () {
   console.log('played the video!');
